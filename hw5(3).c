17d92a0eb74b0f7eb502d946357c362546328e2f
leunk234 <113373828+leunk234@users.noreply.github.com>
2022-10-09 11:38:29 +0900

    Add files via upload

---
+++ hw5(3).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void transformation(int n)
+{
+	if (n == 0 || n == 1)
+		printf("%d", n);
+
+	else {
+		transformation(n / 2);
+		printf("%d", n % 2);
+	}
+}
+
+int main(void)
+{
+	int a;
+
+	printf("please enter a number :");
+	scanf_s("%d", &a);
+	if (a >= 0)
+		transformation(a);
+	else
+		printf("please enter 0or positive number");
+	return 0;
+}
