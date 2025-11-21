21f6469826264ba32be0fd962982d5eeef679d83
leunk234 <113373828+leunk234@users.noreply.github.com>
2025-11-21 18:11:06 +0900

    Add files via upload

---
+++ Homework8.java
@@ -0,0 +1,29 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+public class Homework8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<String,String> loginMap = new HashMap<String, String>();
+        loginMap.put("myId","myPass");
+        loginMap.put("myId2","myPass2");
+        loginMap.put("myId3","myPass3");
+        while(true){
+            System.out.println("id와 password를 입력해주세요.");
+            System.out.printf("id : ");
+            String id = sc.nextLine().trim();
+            if(!loginMap.containsKey(id)){
+                System.out.println("입력하신 id는 존재하지 않습니다. 다시 입력해주세요.");
+                continue;
+            }
+            System.out.printf("password : ");
+            String password = sc.nextLine().trim();
+            if(!loginMap.get(id).equals(password)){
+                System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
+                continue;
+            }
+            System.out.println("id와 비밀번호가 일치합니다.");
+            break;
+        }
+    }
+}
